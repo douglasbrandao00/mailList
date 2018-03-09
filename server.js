2926fce3a7cfd713d15b465391a4426f205f9595
douglasbrandao00 <douglaswfbrandao@gmail.com>
2018-03-09 20:03:38 -0300

    ajustando README

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./src/app.js');
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`app is runing on hels gate ${PORT}`))
