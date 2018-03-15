1069130a2f530232dbb148f2bddf47210f2ab904
douglasbrandao00 <douglaswfbrandao@gmail.com>
2018-03-15 19:52:08 -0300

    modificando deploy

---
+++ src/config/keys.js
@@ -0,0 +1,4 @@
+process.env.NODE_ENV === 'production'
+  ? module.exports = require('./prod')
+  : module.exports = require('./dev')
+
---
+++ src/config/prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+  googleID: process.env.GOOGLE_ID,
+  googleSecret:process.env.GOOOGLE_SECRET,
+  mongodbURI:process.env.MONGO_URI,
+  cookieKey:process.env.COOKIE_KEY, 
+}
